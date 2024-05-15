c7fb946cc9f1600b6c3041d222f38a6fc7358f96
kimmisinha <kimmi.sinha@newput.com>
2024-05-15 17:19:03 +0530

    Hide ,Show and Toggle

--- learning/src/App.js
+++ learning/src/App.js
@@ -4,22 +4,23 @@ import User2 from './User2';
 import { User, User_3 } from './User';
 import { Usernew } from './Usernew';
 import State from './FunctionalState';
- import Usernew_2 from './ClassState';
+import Usernew_2 from './ClassState';
 import Student from './Student';
 import Input from './Input';
- 
+import Toggle from './Toggle'
 function App() {
   return (
     <div className="App">
-    <h1>Hello world</h1>
-    <User></User>
-    <User2></User2>
-    <User_3></User_3>
-    <Usernew></Usernew>
-    <State></State>
-    <Usernew_2></Usernew_2>
-    <Student name="kimmi"/>
-    <input></input>
+      <h1>Hello world</h1>
+      <User></User>
+      <User2></User2>
+      <User_3></User_3>
+      <Usernew></Usernew>
+      <State></State>
+      <Usernew_2></Usernew_2>
+      <Input></Input>
+      <Student name="kimmi" />
+      <Toggle></Toggle>
 
     </div>
   );
--- learning/src/Input.js
+++ learning/src/Input.js
@@ -1,12 +1,25 @@
+import React, { useState } from 'react';
+
 export default function Input() {
-    function getdata(value){
-        console.log(value,"kimmidcdsc")
-    }
-    return (
-        
+    const [data,setData]=useState(null)
+    const [print,setPrint]=useState(false)
+    
+      function getData(val)
+      {
+        console.warn(val.target.value)
+        setData(val.target.value)
+        setPrint(false)
+      }
+      return (
         <div className="App">
-        <h1>Get the Input value</h1>
-        <input type="text" onChange={getdata}></input>
+         {
+           print?
+           <h1> {data}</h1>
+           :null
+         }
+        <input type="text" onChange={getData} />
+        <button onClick={()=>setPrint(true)} >Print Data</button>
         </div>
-    );
+      );
+    
 }

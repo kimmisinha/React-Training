a9e9e1011bec3656b10f2ac39185190b60d65eea
kimmisinha <kimmi.sinha@newput.com>
2024-05-21 14:46:56 +0530

    Lists

--- Assignment/assignment/src/Component/Lists.jsx
+++ Assignment/assignment/src/Component/Lists.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-function List(){
+function List() {
+    const items = ['apple', 'mango', 'orange']
+    return (
+        <>
+            {items.map((item, index) => (
+            <li key={index}>{item}</li>
+            ))}
+        </>
 
-   return ( 
-    <h1>Hello</h1>
-  )
+    )
 }
 export default List;

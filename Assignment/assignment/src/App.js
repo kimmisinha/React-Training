8b11f222003835807f7af2aad78b5cb3644dcc49
kimmisinha <kimmi.sinha@newput.com>
2024-05-21 14:40:48 +0530

    Batching

--- Assignment/assignment/src/App.js
+++ Assignment/assignment/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import ClassMounting from './Component/ClassMounting';
 import FunctionalMounting from './Component/FunctionalMounting';
 import Events from './Component/Events';
+import Batching from './Component/Batching';
+import Keys from './Component/Keys';
+import Lists from './Component/Lists'
 function App() {
   const userName = 'John Doe';
   const userAge = 30;
@@ -12,6 +15,9 @@ function App() {
       <ClassMounting data={[userName, userAge]} />
       <FunctionalMounting />
       <Events/>
+      <Batching/>
+      <Keys/>
+      <Lists/>
     </div>
   );
 }
---
+++ Assignment/assignment/src/Component/Batching.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+function Batching() {
+  let [count, setcount] = useState(0);
+  const [text, setext] = useState(0);
+
+  function handleclick() {
+    setcount(count + 1);
+    setext(text + 1);
+    // React batches the above two state updates into a single update
+
+  }
+  return (
+    <div>
+
+      <button onClick={handleclick}>Button</button>
+      <p>count:{count}</p>
+      <p>text:{text}</p>
+    </div>
+
+
+  )
+}
+export default Batching;
+
+/*
+React automatically batches multiple state updates into a single update
+for performance optimization, even in functional components.
+*/
